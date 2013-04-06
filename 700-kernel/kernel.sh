d93bd9260ccc0aff5e1fe20a257f5174255fe523
DHO <DHO@dho.im>
2013-04-06 15:09:21 -0400

    Fix kernel script.
    Change-Id: Id2019e3bc5d8c8092a7b576f955b2c63a1ff6c36
    Signed-off-by: DHO <DHO@dho.im>

--- 700-kernel/kernel.sh
+++ 700-kernel/kernel.sh
@@ -1,37 +1,20 @@
 #!/bin/bash
 # To be run from the root of the build tree.
-# NOT FROM THE PLATFORM_MANIFEST DIRECTORY!!!
+# NOT FROM THE manifest DIRECTORY!!!
 # -KhasMek
 
-manifest=.repo/local_manifests/kernel_manifest.xml
+manifest=.repo/manifests/700-kernel/kernel_manifest.xml
 
 echo "adding kernel repos to your build tree"
 
-if [ ! -d ".repo/local_manifests" ]; then
-    mkdir ".repo/local_manifests"
-fi
-
-# Safely merge depricated local_manifest.xml
-if [ -f ".repo/local_manifest.xml" ]; then
-    mv .repo/local_manifest.xml .repo/local_manifests/local_manifest.xml
-    tail -n +3 platform_manifest/kernel_manifest.xml | head -n -1 | while read line
-    do
-        # See if there are dublicate entries, remove from local_manifest if true
-        chickenfucker=`grep "$line" .repo/local_manifests/local_manifest.xml`
-        if [ -n "$chickenfucker" ]; then
-            fuckedchicken=$(echo "$line" |sed 's!\(\/\)!\\\1!g')
-            sed -i "/$fuckedchicken/d" .repo/local_manifests/local_manifest.xml
-            if [ `cat .repo/local_manifests/local_manifest.xml | wc -l` = 3 ]; then
-                rm .repo/local_manifests/local_manifest.xml
-            fi
-        fi
-    done
+if [ ! -d ".repo/manifests" ]; then
+    mkdir ".repo/manifests"
 fi
 
 if [ -f $manifest ]; then
         echo "kernel manifest already present."
         echo "merging kernel manifests."
-        cat platform_manifest/kernel_manifest.xml | while read line
+        cat manifest/700-kernel/kernel_manifest.xml | while read line
         do
                 kernel=`grep "$line" $manifest`
                 if [ -z "$kernel" ]; then
@@ -44,9 +27,9 @@ if [ -f $manifest ]; then
         mv '$manifest'.new $manifest
 fi
 
-cp platform_manifest/kernel_manifest.xml $manifest
+cp manifest/700-kernel/kernel_manifest.xml $manifest
 echo "syncing kernel repos"
-tail -n +3 platform_manifest/kernel_manifest.xml | head -n -1 | cut -f2 -d '"' > .sync
+tail -n +3 manifest/700-kernel/kernel_manifest.xml | head -n -1 | cut -f2 -d '"' > .sync
 
 . build/envsetup.sh
 
