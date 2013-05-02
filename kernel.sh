2795b68bd14025ac16e01a111dd2fcc23f951033
DHOMD <DHO@dho.im>
2013-05-01 20:52:30 -0400

    Minor OCD tweaks
    Change-Id: I17338bff58fdd34446d03a4af1b37abb4622b5d1

--- kernel.sh
+++ kernel.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Uncomment out desired kernels
+# Uncomment desired kernels
 # Run from ROOT directory
 # DHO
 
 # TF700t
 # git clone git://github.com/CyanogenMod/android_kernel_asus_tf700t kernel/asus/tf700t
 
-#Mako
+# Mako
 git clone git://github.com/faux123/mako kernel/lge/mako
