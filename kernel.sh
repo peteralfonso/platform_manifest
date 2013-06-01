f6b29d236b44a2e342b4a9ead6f407e17c1dabda
DHOMD <DHO@dho.im>
2013-06-01 17:22:23 -0400

    Update kernel.sh
    Include i605, commend out mako
    
    Change-Id: Iab62ad74798d71b114d6e29edac00cac5d07fe91
    Signed-off-by: DHOMD <DHO@dho.im>

--- kernel.sh
+++ kernel.sh
@@ -4,7 +4,10 @@
 # DHO
 
 # TF700t
-# git clone git://github.com/CyanogenMod/android_kernel_asus_tf700t kernel/asus/tf700t
+# git clone -b cm-10.1 --single-branch git://github.com/CyanogenMod/android_kernel_asus_tf700t kernel/asus/tf700t
 
 # Mako
-git clone git://github.com/faux123/mako kernel/lge/mako
+#git clone git://github.com/faux123/mako kernel/lge/mako
+
+# Note 2
+git clone -b cm-10.1 --single-branch git://github.com/CyanogenMod/android_kernel_samsung_smdk4412 kernel/samsung/smdk4412 
