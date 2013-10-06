3a06974d29645b452e4b42ea0811ed07721feb29
nuclearmistake <nuclearmistake@gmail.com>
2013-10-06 11:56:59 -0400

    INTRODUCING: Vanir BOTTLESERVICE
    Change-Id: I963dac20b7068beaec6c75dae161366a052156b2

---
+++ kernel_special_cases.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#this is brittle, but it ought to get the job done.
+
+## all non-specified kernels are assumed to be from CyanogenMod
+
+case $1 in
+    m7*)
+        export remote="vanir"
+        export remoterevision="sonic-dev"
+    ;;
+    toro|tuna|maguro)
+        export remote="github"
+        export remoterevision="jim_mr2"
+        export kernelsource="jimsth/vanir_tuna"
+    ;;
+    vigor*)
+        export remote="github"
+        export remoterevision="cm-10.2"
+        export kernelsource="Flyhalf205/vigor_aosp_kernel"
+    ;;
+    p880)
+        export remote="github"
+        export remoterevision="cm-10.2"
+        export kernelsource="mzhou/lge-kernel-p880-cyanogenmod"
+    ;;
+    e975|e980)
+        export kernelsource="lge-kernel-gproj"
+    ;;
+esac
