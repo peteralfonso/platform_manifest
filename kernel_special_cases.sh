175e7c0021771718cdc381d92873c4fc7d52170d
DHOMD <DHO@dho.im>
2013-10-09 14:37:04 -0400

    Homina Homina Honami
    Change-Id: Iebb5dd8a7d9d420b26ca4faffe95c9cb37fdac67

--- kernel_special_cases.sh
+++ kernel_special_cases.sh
@@ -4,9 +4,22 @@
 ## all non-specified kernels are assumed to be from CyanogenMod
 
 case $1 in
+    e975|e980)
+        export kernelsource="lge-kernel-gproj"
+    ;;
+    honami)
+        export remote="github"
+        export remoterevision="cm-10.2"
+        export kernelsource="MightyBear007/android_kernel_sony_msm8974"
+    ;;
     m7*)
         export remote="vanir"
         export remoterevision="sonic-dev"
+    ;;
+    p880)
+        export remote="github"
+        export remoterevision="cm-10.2"
+        export kernelsource="mzhou/lge-kernel-p880-cyanogenmod"
     ;;
     toro|toroplus|maguro)
         export remote="github"
@@ -17,13 +30,5 @@ case $1 in
         export remote="github"
         export remoterevision="cm-10.2"
         export kernelsource="Flyhalf205/vigor_aosp_kernel"
-    ;;
-    p880)
-        export remote="github"
-        export remoterevision="cm-10.2"
-        export kernelsource="mzhou/lge-kernel-p880-cyanogenmod"
-    ;;
-    e975|e980)
-        export kernelsource="lge-kernel-gproj"
     ;;
 esac
