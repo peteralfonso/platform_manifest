c299b8dc43021165b59bf38b166a031d61eb804d
DHOMD <DHO@dho.im>
2013-12-09 18:19:41 -0500

    Remove Ovation, it will not be raised to kk44.  Never worked on jb43
    Change-Id: I3a36e6475ef95c12c54b56ab0a985c0c88a4061c

--- kernel_special_cases.sh
+++ kernel_special_cases.sh
@@ -27,9 +27,6 @@ case $1 in
         export remoterevision="sonic-dev"
         export kernelsource="kernel_htc_m7"
     ;;
-    ovation)
-        export remoterevision="cm-10.2"
-    ;;
     p3100|p3110|p5100|p5110)
         export remoterevision="cm-10.2"
     ;;
