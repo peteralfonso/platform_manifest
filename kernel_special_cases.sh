0590899bda7c936a2b8328a5fc731494d233aa95
DHOMD <DHO@dho.im>
2014-02-11 16:53:10 -0500

    D710: Use a kernel designed for Vanir.  Thank You mtb3000gt
    Change-Id: If63cc6ef1ec9f1f4a6cec17dcc00d205f5e992aa
    Signed-off-by: DHOMD <DHO@dho.im>

--- kernel_special_cases.sh
+++ kernel_special_cases.sh
@@ -13,8 +13,8 @@ case $1 in
     ;;
     d710)
         export remote="github"
-        export remoterevision="kk_mkt"
-        export kernelsource="MoKee/android_kernel_samsung_d710"
+        export remoterevision="cm-11.0"
+        export kernelsource="mtb3000gt/CM_android_kernel_samsung_smdk4412"
     ;;
     d80*|ls980|vs980)
         export remote="vanir"
