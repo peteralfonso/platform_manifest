1720d1c8eb160596648bb799532e3e3d2081031b
nuclearmistake <nuclearmistake@gmail.com>
2014-04-04 01:46:47 -0400

    kernelderp
    Change-Id: Icd2ca2b0fcf8c132e400622b9dbdd103e7418d2a

--- kernel_special_cases.sh
+++ kernel_special_cases.sh
@@ -7,7 +7,7 @@ export remoterevision="cm-11.0"
 
 case $1 in 
     anzu|coconut|haida|hallon|iyokan|mango|satsuma|smultron|urushi)
-        export kernelsource="LegacyXperia/android_kernel_msm7x30"
+        export kernelsource="LegacyXperia/android_kernel_semc_msm7x30"
         export remote="lx"
         export remoterevision="semc-wip"
     ;;
